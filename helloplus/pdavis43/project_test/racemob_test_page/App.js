d550d5d0bd5e0b78813d9994f32afcf4efddd1e9
Peyton Davis <davisapeyton@gmail.com>
2023-09-22 18:38:54 -0400

    submission for helloplus
    using react native and expo to see / live-edit the frontend

---
+++ helloplus/pdavis43/project_test/racemob_test_page/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the beginning of RaceMob 9/22/23 18:28!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
