775d55740ba1fc1e1d866f325e871cde555bce1b
CalebKornegay <caleb.kornegay@gmail.com>
2023-10-03 17:36:15 -0400

    #49 linting script for windows

--- app/frontend/burnout/lint.bat
+++ app/frontend/burnout/lint.bat
@@ -1,1 +1,36 @@
 @echo off
+
+if exist errors.txt (
+    set /p yn="Would you like me to delete errors.txt (y|n)? "
+    if "%yn%"=="n" (
+        exit /b 1
+    ) else (
+        call del errors.txt
+    )
+)
+
+set /p yn="Would you like to run the linter with the --fix option (y|n)? "
+set fix=""
+
+if "%yn%"=="y" (
+    set fix="--fix"
+)
+
+set dirs=components screens .
+
+(for %%a in (%dirs%) do (
+    for %%i in (%%a\*.js) do (
+        if not "%%i" == ".\babel.config.js" (
+            if "%fix%"=="--fix" (
+                call npx eslint %fix% %cd%\%%i >> errors.txt 2>&1
+            ) else (
+                call npx eslint %cd%\%%i >> errors.txt 2>&1
+            )     
+            echo %cd%\%%i
+            echo: >> errors.txt
+            echo: >> errors.txt
+        )
+    )
+))
+echo:
+echo Check errors.txt for the list of errors
