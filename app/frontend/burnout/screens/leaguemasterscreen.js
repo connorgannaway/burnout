eb77d6a77fae5e43dee1f5d8fea99ec352fc1b15
Andrew Lindstrom <alindstr@vols.utk.edu>
2023-10-04 19:31:52 -0400

    Created master screen to display league data #63

--- app/frontend/burnout/screens/leaguemasterscreen.js
+++ app/frontend/burnout/screens/leaguemasterscreen.js
@@ -1,16 +1,52 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import { buildPageCards } from './pageElements';
+import { View, ScrollView, Dimensions, StyleSheet } from 'react-native';
+import { buildCardsFromData } from '../components/card'
+import BottomBar from '../components/bottombar';
+import Topbar from '../components/topbar';
 
-const pageList = ['page1', 'page2', 'page3', 'page4'];
 
-export default function LeagueMasterScreen({navigation}){
+/*
+ * LeagueMasterScreen component builds a screen that displays information about the league
+ *		props:
+ *			navigation: navigation prop created by React Navigation
+ *
+ */
+export default class LeagueMasterScreen extends React.Component{
+	constructor(){
+		super()
+		this.state = {
+			showComponent: true,
+		}
+	}
 
-	const cards = buildPageCards(navigation, pageList);
+	render() {
+		const {navigation} = this.props;
+		/*TODO: swap test data with an API call to retrieve real data*/
+		const drivers = [{name: "Driver 1", subName: "Constructor 1", body: "Race Data", bgcolor: "#5f5f5f", where: "drivermasterscreen"},
+						 {name: "Driver 2", subName: "Constructor 2", body: "Race Data", bgcolor: "#5f5f5f", where: "drivermasterscreen"},
+						 {name: "Driver 3", subName: "Constructor 3", body: "Race Data", bgcolor: "#5f5f5f", where: "drivermasterscreen"}];
 
-	return(
-		<View>
-            
-		</View>
-	);
+		const driverCards = buildCardsFromData(navigation, drivers);
+
+		return(
+			<View style={styles.container}>
+				<Topbar />
+				<ScrollView>
+					{driverCards}
+				</ScrollView>
+				<BottomBar navigation={navigation}/>
+			</View>
+		);
+	}
 }
+
+const screen = Dimensions.get('screen');
+const styles = StyleSheet.create({
+	container:{
+		flex: 1,
+		justifyContent: 'center',
+		height: screen.height-250,
+		width: screen.width,
+		backgroundColor: "#efefef",
+	},
+})
