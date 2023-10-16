f5b7fd0c9cac1746622ace753697a3b920aee7b9
CalebKornegay <caleb.kornegay@gmail.com>
2023-10-16 10:29:30 -0400

    l

---
+++ app/frontend/burnout/api/urls.js
@@ -0,0 +1,4 @@
+export const BASEURL='https://pitwall.connorgannaway.net';
+// APIURL export everywhere so we can concatenate extensions and variables to query api
+
+export const V1MESSAGESJSON=BASEURL+'/v1/messages/?format=json';
