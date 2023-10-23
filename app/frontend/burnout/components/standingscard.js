30c6c1f20c271af41d6c0da16e8b01a8cceb6f63
codyallen9202 <codyman9202@gmail.com>
2023-10-22 21:35:47 -0400

    early version of card to show top 5 in standings on LMS

--- app/frontend/burnout/components/standingscard.js
+++ app/frontend/burnout/components/standingscard.js
@@ -1,21 +1,69 @@
 import * as React from 'react';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import { Card, Title } from 'react-native-paper';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
-
-const StandingsCard = () => (
-  <Card>
-    <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+const StandingsCard = ({ data }) => (
+  <Card style={styles.container}>
     <Card.Content>
-      <Text variant="titleLarge">Card title</Text>
-      <Text variant="bodyMedium">Card content</Text>
+      <Title style={styles.title}>Standings</Title>
     </Card.Content>
-    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
-    </Card.Actions>
+    <Card.Content style={styles.content}>
+      <View style={styles.rowsContainer}>
+        {data.map((driver, index) => (
+          <View key={index} style={styles.row}>
+            <View style={styles.leftTextContainer}>
+              <Text style={styles.leftText}>{driver.position}. {driver.name}</Text>
+            </View>
+            <View style={styles.rightTextContainer}>
+              <Text style={styles.rightText}>Points: {driver.points}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </Card.Content>
   </Card>
 );
+
+const screen = Dimensions.get('screen');
+const styles = StyleSheet.create({
+  container: {
+    width: screen.width / 1.15,
+    padding: screen.width / 24,
+    marginTop: screen.height / 96,
+    marginBottom: screen.height / 96,
+    borderRadius: 15,
+    backgroundColor: '#ff1801',
+  },
+  title: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+  },
+  rowsContainer: {
+    width: '100%',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    color: 'white',
+    marginBottom: 8,
+  },
+  leftTextContainer: {
+    flex: 1,
+  },
+  rightTextContainer: {
+    flex: 1,
+  },
+  leftText: {
+    color: 'white',
+  },
+  rightText: {
+    color: 'white',
+  },
+});
 
 export default StandingsCard;
--- app/frontend/burnout/screens/leaguemasterscreen.js
+++ app/frontend/burnout/screens/leaguemasterscreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { View, ScrollView, Dimensions, StyleSheet } from 'react-native';
+import { View, ScrollView, Dimensions, StyleSheet, Button } from 'react-native';
 import { buildCardsFromData } from '../components/card';
 import BottomBar from '../components/bottombar';
 import Topbar from '../components/topbar';
+import StandingsCard, { standingsCard } from '../components/standingscard';
 
 
 /*
@@ -28,16 +29,22 @@ export default class LeagueMasterScreen extends React.Component{
 
 		const schedule = [{name: 'Schedule', body:'10/8     Next Race\n10/15   Nexter Race\n10/22   Nextest Race', bgcolor: '#ff1801', where:'TestScreen'}];
 		const scheduleCard = buildCardsFromData(navigation, schedule);
-
-		const standings = [{name: 'Standings', body:'1. Max Verstappen - All the points\n1. Driver 2 - X Points\n1. Driver 3 - X Points', bgcolor: '#ff1801', where:'TestScreen'}];
-		const standingsCard = buildCardsFromData(navigation, standings);
+		
+		const standingsData = [
+			{ position: 1, name: 'Driver 1', team: 'Team A', points: 100 },
+			{ position: 2, name: 'Driver 2', team: 'Team B', points: 90 },
+			{ position: 3, name: 'Driver 3', team: 'Team C', points: 80 },
+			{ position: 4, name: 'Driver 4', team: 'Team D', points: 75 },
+			{ position: 5, name: 'Driver 5', team: 'Team E', points: 70 },
+			// Add data for all teams
+		  ];
 
 		return(
 			<View style={styles.container}>
 				<ScrollView>
 					{raceRecapCard}
 					{scheduleCard}
-					{standingsCard}
+					<StandingsCard data={standingsData} />
 				</ScrollView>
 			</View>
 		);
