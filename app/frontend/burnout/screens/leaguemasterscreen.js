e96f8c524aaff23f62ea6484d6c180e1c4287a9b
Andrew Lindstrom <alindstr@vols.utk.edu>
2023-10-25 19:14:22 -0400

    created basic table component #101

--- app/frontend/burnout/screens/leaguemasterscreen.js
+++ app/frontend/burnout/screens/leaguemasterscreen.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { View, ScrollView, Dimensions, StyleSheet } from 'react-native';
-import { buildCardsFromData } from '../components/card';
+import BaseCard, { buildCardsFromData } from '../components/card';
+import { Table } from '../components/table';
 import BottomBar from '../components/bottombar';
 import Topbar from '../components/topbar';
 
@@ -21,24 +22,15 @@ export default class LeagueMasterScreen extends React.Component{
 
 	render() {
 		const {navigation} = this.props;
-		/*TODO: swap test data with an API call to retrieve real data*/
-		
-		const raceRecap = [{name: 'Last Race', body:'1. Max Verstappen\n2. Lightning McQueen\n3. Spider-Man', bgcolor: '#ff1801', where:'TestScreen'}];
-		const raceRecapCard = buildCardsFromData(navigation, raceRecap);
-
-		const schedule = [{name: 'Schedule', body:'10/8     Next Race\n10/15   Nexter Race\n10/22   Nextest Race', bgcolor: '#ff1801', where:'TestScreen'}];
-		const scheduleCard = buildCardsFromData(navigation, schedule);
-
-		const standings = [{name: 'Standings', body:'1. Max Verstappen - All the points\n1. Driver 2 - X Points\n1. Driver 3 - X Points', bgcolor: '#ff1801', where:'TestScreen'}];
-		const standingsCard = buildCardsFromData(navigation, standings);
+		const DATA = [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0];
 
 		return(
 			<View style={styles.container}>
-				<ScrollView>
-					{raceRecapCard}
-					{scheduleCard}
-					{standingsCard}
-				</ScrollView>
+				<Table
+					data={DATA}
+					numColumns={3}
+					navigation={navigation}
+				/>
 			</View>
 		);
 	}
@@ -48,7 +40,6 @@ const screen = Dimensions.get('screen');
 const styles = StyleSheet.create({
 	container:{
 		flex: 1,
-		justifyContent: 'center',
 		height: screen.height-250,
 		width: screen.width,
 		backgroundColor: '#efefef',
