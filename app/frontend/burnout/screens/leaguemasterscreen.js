743c260c246e06aee1b24dbdd1bce6d3f1a4c26f
Andrew Lindstrom <alindstr@vols.utk.edu>
2023-11-04 15:17:27 -0400

    added functionality to populate leaguemasterscreen tables with server data (#130)

--- app/frontend/burnout/screens/leaguemasterscreen.js
+++ app/frontend/burnout/screens/leaguemasterscreen.js
@@ -8,12 +8,11 @@
 import * as React from 'react';
 import { View, ScrollView, Dimensions, StyleSheet, Text } from 'react-native';
 import BaseCard, { buildCardsFromData } from '../components/card';
-import { Table, ScrollTable, TableManager } from '../components/table';
-import BottomBar from '../components/bottombar';
-import Topbar from '../components/topbar';
-
 import { Table, TableManager } from '../components/table';
 import StandingsCard, { standingsCard } from '../components/standingscard';
+import { getLeagues } from "../api/leagues";
+import { getConstructorDetails, getDriverDetails } from "../api/leaguedetails";
+
 
 const screen = Dimensions.get('screen');
 const styles = StyleSheet.create({
@@ -30,136 +29,81 @@ const styles = StyleSheet.create({
  * LeagueMasterScreen component builds a screen that displays information about the league
  *		props:
  *			navigation: navigation prop created by React Navigation
- *
  */
 export default class LeagueMasterScreen extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			showComponent: true,
+			isLoadingConstructors: true,
+			isLoadingDrivers: true,
+			constructorDetails: getConstructorDetails(),
+			driverDetails: getDriverDetails(),
 		};
+
+		this.state.constructorDetails
+			.catch(error => {
+				console.warn(error);
+			}).then(() => {
+				this.setState({isLoadingConstructors: false});
+			}).catch(error => {
+				console.warn(error);
+			});
+
+		this.state.driverDetails
+			.catch(error => {
+				console.warn(error);
+			}).then(() => {
+				this.setState({isLoadingDrivers: false});
+			}).catch(error => {
+				console.warn(error);
+			});
+	}
+
+	shouldComponentUpdate(nextState){
+		if(this.state.constructorDetails != nextState.constructorDetails) return true;
+		if(this.state.driverDetails != nextState.driverDetails) return true;
+		if(nextState.isLoadingDrivers === false) return true;
+		if(nextState.isLoadingConstructors === false) return true;
+
+		return false;
+	}
+
+	put(data, loadCheck){
+		console.log(data);
+
+		if(data != null){
+			if(loadCheck){
+				const c = [];
+				for(let i = 0; i < data.length/2; i++){
+					c.push(data[i]);
+				}
+				return c;
+			} else return data;
+		}
+
+		return [];
 	}
 
 	render() {
 		const { navigation } = this.props;
-		const DATA = [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4];
-		/*TODO: swap test data with an API call to retrieve real data*/
-		
-		const raceRecap = [{name: 'Last Race', body:'1. Max Verstappen\n\
-                            2. Lightning McQueen\n3. Spider-Man', bgcolor: '#ff1801', where:'RaceScreen'}];
-		const raceRecapCard = buildCardsFromData(navigation, raceRecap);
-
-		const schedule = [{name: 'Schedule', body:'10/8     Next Race\n\
-                            10/15   Nexter Race\n10/22   Nextest Race', bgcolor: '#ff1801', where:'TestScreen'}];
-		const scheduleCard = buildCardsFromData(navigation, schedule);
-		
-		const standingsData = [
-			{ position: 1, name: 'Driver 1', team: 'Team A', points: 100 },
-			{ position: 2, name: 'Driver 2', team: 'Team B', points: 90 },
-			{ position: 3, name: 'Driver 3', team: 'Team C', points: 80 },
-			{ position: 4, name: 'Driver 4', team: 'Team D', points: 75 },
-			{ position: 5, name: 'Driver 5', team: 'Team E', points: 70 },
-			// Add data for all teams
-		];
-
-		// const teams = [{name: 'Teams', body: 'Click to view individual team page',
-        //                     bgcolor: '#ff1801', where: 'TeamMasterScreen'}];
-		// const teamscard = buildCardsFromData(navigation, teams);
 
 		return(
 			<View style={styles.container}>
-				<ScrollView>
-					<TableManager headings = {["first", "second","third","fourth"]}>
-						<ScrollTable
-							key={0}
-							data={DATA}
-							numColumns={1}
-							navigation={navigation}
-						/>
+				<ScrollView snapToAlignment='center'>
+					<TableManager headings = {['Constructors', 'Drivers']}>
 						<Table
 							key={1}
-							data={DATA}
-							numColumns={2}
+							data={this.put(this.state.constructorDetails['_j'], this.isLoadingConstructors)}
+							numColumns={4}
 							navigation={navigation}
 						/>
 						<Table
 							key={2}
-							data={DATA}
-							numColumns={3}
-							navigation={navigation}
-						/>
-						<Table
-							key={3}
-							data={DATA}
+							data={this.put(this.state.driverDetails['_j'], this.isLoadingDrivers)}
 							numColumns={4}
 							navigation={navigation}
 						/>
 					</TableManager>
-					<BaseCard 
-						navigation={navigation}
-						name={'Formula 1'}
-						body={'Click to view the Formula 1 League Page'}
-						bgcolor={'#ff1801'}
-						where={'LeagueMasterScreen'}
-					/>
-					<BaseCard 
-						navigation={navigation} 
-						name={'NASCAR'}
-						body={'Click to view the NASCAR League Page'}
-						bgcolor={'#e4002b'}
-						where={'LeagueMasterScreen'}
-					/>
-					<BaseCard 
-						navigation={navigation} 
-						name={'MotoGP'}
-						body={'Click to view the MotoGP League Page'}
-						bgcolor={'#E0144C'}
-						where={'LeagueMasterScreen'}
-					/>
-					<BaseCard 
-						navigation={navigation} 
-						name={'IndyCar'}
-						body={'Click to view the IndyCar League Page'}
-						bgcolor={'#b92a30'}
-						where={'LeagueMasterScreen'}
-					/>
-				</ScrollView>
-				<TableManager headings = {['first', 'second','third']}>
-					<Table
-						key={1}
-						data={DATA}
-						numColumns={2}
-						navigation={navigation}
-					/>
-					<Table
-						key={2}
-						data={DATA}
-						numColumns={3}
-						navigation={navigation}
-					/>
-					<Table
-						key={3}
-						data={DATA}
-						numColumns={4}
-						navigation={navigation}
-					/>
-				</TableManager>
-				<ScrollView>
-					{raceRecapCard}
-					{scheduleCard}
-					{standingsCard}
-					<StandingsCard data={standingsData} />
-					<BaseCard
-						name={'Team Name'}
-						subName={'Total Team Points: 9'}
-						body={'Current Team Standing: First'}
-						bgcolor={'#ff1801'}
-						where={'TeamMasterScreen'}
-						navigation={navigation}
-						title={'Team Name'}
-						id={9}
-					/>
-					<StandingsCard data={standingsData} />
 				</ScrollView>
 			</View>
 		);
